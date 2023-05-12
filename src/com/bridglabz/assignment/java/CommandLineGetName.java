edeb9ec7e8cc519e3d5306d2177ff79b5e43c77a
sachinwb <sachinwb1993@gmail.com>
2023-05-13 02:49:39 +0530

    Adding:Second Assignment Display all primitive data type's default value

---
+++ src/com/bridglabz/assignment/java/CommandLineGetName.java
@@ -0,0 +1,15 @@
+package com.bridglabz.assignment.java;
+
+public class CommandLineGetName {
+	
+	
+    //To get the name using the command line.
+	public static void main(String[] args) {
+		for (int i =0;i<args.length;i++)
+		{
+			System.out.println(args[i]);
+		}
+
+	}
+
+}
--- src/com/bridglabz/assignment/java/DisplayPrimitiveDataType.java
+++ src/com/bridglabz/assignment/java/DisplayPrimitiveDataType.java
@@ -2,20 +2,24 @@ package com.bridglabz.assignment.java;
 
 public class DisplayPrimitiveDataType {
 	
+    byte b ;
+	static short s ;
+	static int i ;
+	static long l ;
+	static float f ;
+	static double d ;
+	static char ch ;
+	static boolean bn ;
+	
 	
     //Write a Java program to display the default value of all primitive data types of Java
 	public static void main(String[] args) {
-		byte b =123;
-		short s =135;
-		int i = 12345;
-		long l = 12345678901l;
-		float f = 123.456f;
-		double d =123.465;
-		char ch = 'a';
-		boolean bn =true;
+		DisplayPrimitiveDataType d = new DisplayPrimitiveDataType();
 		
-		System.out.println("The byte primitive data type is : "+b);
-		System.out.println("The short primitive data type is : "+s);
+		
+		
+		System.out.println("The byte primitive data type is : "+d.b);
+		System.out.println("The short primitive data type is : "+d.s);
 		System.out.println("The int primitive data type is : "+i);
 		System.out.println("The long primitive data type is : "+l);
 		System.out.println("The float primitive data type is : "+f);
