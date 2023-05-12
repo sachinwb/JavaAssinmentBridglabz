533fa7d430422df81507b878f39e02fa20bd5336
sachinwb <sachinwb1993@gmail.com>
2023-05-13 01:07:22 +0530

    adding:Third Assignment Check String Equals or not

---
+++ src/com/bridglabz/assignment/java/CheckStringEqual.java
@@ -0,0 +1,19 @@
+package com.bridglabz.assignment.java;
+
+public class CheckStringEqual {
+	
+	
+    //Write a program to check two strings are equal or not
+	public static void main(String[] args) {
+		String s1 = "SaChin";
+		String s2 ="sachin";
+		String s3 ="SaChin";
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equalsIgnoreCase(s2));
+		
+
+	}
+
+}
