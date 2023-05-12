c07bed7a16ff76a818e0fd3c37c602ec0eb0332c
sachinwb <sachinwb1993@gmail.com>
2023-05-13 00:55:56 +0530

    adding:Second assingment display all primitive data type

--- src/com/bridglabz/assignment/java/DisplayMessage.java
+++ src/com/bridglabz/assignment/java/DisplayMessage.java
@@ -1,7 +1,9 @@
 package com.bridglabz.assignment.java;
 
 public class DisplayMessage {
-
+	
+	
+    // Write a program to display any message
 	public static void main(String[] args) {
 		
 		System.out.println("hello world...");
---
+++ src/com/bridglabz/assignment/java/DisplayPrimitiveDataType.java
@@ -0,0 +1,28 @@
+package com.bridglabz.assignment.java;
+
+public class DisplayPrimitiveDataType {
+	
+	
+    //Write a Java program to display the default value of all primitive data types of Java
+	public static void main(String[] args) {
+		byte b =123;
+		short s =135;
+		int i = 12345;
+		long l = 12345678901l;
+		float f = 123.456f;
+		double d =123.465;
+		char ch = 'a';
+		boolean bn =true;
+		
+		System.out.println("The byte primitive data type is : "+b);
+		System.out.println("The short primitive data type is : "+s);
+		System.out.println("The int primitive data type is : "+i);
+		System.out.println("The long primitive data type is : "+l);
+		System.out.println("The float primitive data type is : "+f);
+		System.out.println("The double primitive data type is : "+d);
+		System.out.println("The char primitive data type is : "+ch);
+		System.out.println("The boolean primitive data type is : "+bn);
+
+	}
+
+}
