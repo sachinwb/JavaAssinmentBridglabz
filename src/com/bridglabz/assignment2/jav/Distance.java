664146ac2509809f911d406691bbd96719a8d6e4
sachinwb <sachinwb1993@gmail.com>
2023-05-14 01:53:37 +0530

    aster

--- src/com/bridglabz/assignment2/jav/Distance.java
+++ src/com/bridglabz/assignment2/jav/Distance.java
@@ -5,6 +5,7 @@
 
 package com.bridglabz.assignment2.jav;
 import java.util.Scanner;
+import java.lang.Math;
 public class Distance {
 
 	public static void main(String[] args) {
@@ -19,7 +20,7 @@ public class Distance {
 		int y = Integer.parseInt(args[1]);
 		
 		
-		double distance = Math.sqrt((x*x)+(y*y));
+		double distance = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
 		
 		System.out.println("Euclidean distance is : "+distance);
 
