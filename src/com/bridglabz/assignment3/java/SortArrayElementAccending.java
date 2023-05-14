dd7dd6c6052fc84263ec44b16e2b2fd76e6a7c85
sachinwb <sachinwb1993@gmail.com>
2023-05-15 03:07:13 +0530

    Updated:SortArrayElement assignment

--- src/com/bridglabz/assignment3/java/SortArrayElementAccending.java
+++ src/com/bridglabz/assignment3/java/SortArrayElementAccending.java
@@ -1,16 +1,30 @@
 
 //Java Program to sort the elements of an array in ascending order 
 package com.bridglabz.assignment3.java;
-import java.util.Arrays;
+//import java.util.Arrays;
 public class SortArrayElementAccending {
 
 	public static void main(String[] args) {
 		int num[]= {10,2,8,4,9,3,7,5,6,1};
 
-		Arrays.sort(num);
-		for(int x:num) {
-		System.out.print(x+" ");
+		int arr;
+		for(int i =0;i<num.length;i++)
+		{
+			for(int j =i ;j<num.length;j++)
+			{
+				if(num[i]>num[j])
+				{
+					arr=num[i];
+					num[i]=num[j];
+					num[j]=arr;
+				}
+			}
 		}
+	  for(int x:num) {
+		  System.out.print(x+" ");
+	  }
+		
+			
 	}
 
 }
